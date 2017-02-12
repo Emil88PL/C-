bc7df7ec618dfbe53cc6f7c28296fad3828bcd14
Emil <emil88pl@gmail.com>
2017-02-12 09:16:39 +0000

    Add files via upload

---
+++ inserting to a string/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string napis="Ala ma kota";
+
+    napis.insert(11," o imieniu Zdzisiek");
+
+    cout<<napis;
+
+
+
+    return 0;
+}
