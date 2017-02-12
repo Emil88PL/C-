82b2045b366aea8227710edc75a7b805742e9a60
Emil <emil88pl@gmail.com>
2017-02-12 09:13:36 +0000

    Add files via upload

---
+++ fanding phrase/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string napis="Ala ma kota";
+    string szukaj="kot";
+
+    size_t pozycja=napis.find(szukaj);
+
+    if(pozycja!=string::npos)
+        cout<<"Znaleziono na pozycji: "<<pozycja;
+    else cout<<"Nie znaleziono!";
+
+
+    return 0;
+}
