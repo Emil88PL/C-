1ed7a5f924c3deacc35d8613f831ec8d007a2b92
Emil <emil88pl@gmail.com>
2017-02-15 19:07:52 +0000

    Add files via upload

---
+++ concatenacja/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string> //biblioteka do chyba laczenia stringow contatenacjii
+using namespace std;
+
+int main()
+{
+    string jeden="ala ma ";
+    string dwa="kota";
+
+    string trzy=jeden+dwa;
+    cout<<trzy;
+
+
+
+
+    return 0;
+}
