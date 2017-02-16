599f1ddd259a24a6b22832d049119431c90a9562
Emil <emil88pl@gmail.com>
2017-02-16 19:57:24 +0000

    Add files via upload

---
+++ Rekurencja fibionacciego/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int f(int n)
+{
+    if(n==0)return 3;
+    else return f(n-1)+2;
+}
+
+long int potega(int p,int w)
+{
+    if (w==0) return 1;
+    else return p*potega(p, w-1);
+}
+
+long int fib(int n)
+{
+   if(n==1 || n==2) return 1;
+    else return fib(n-1)+fib(n-2);
+}
+
+int main()
+{
+    cout << f(3) << endl;
+    cout << potega(3,4) << endl;
+    cout << fib(55)<< endl;
+
+
+    return 0;
+}
